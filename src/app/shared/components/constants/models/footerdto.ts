318e06f907bc17a342cb1e05588ac757414e0b1c
ah <ah@fadsf.com>
2025-05-05 18:21:59 +0500

    init

---
+++ src/app/shared/components/constants/models/footerdto.ts
@@ -0,0 +1,11 @@
+interface MenuItem {
+    name: string;
+    link: string;
+  }
+  
+ export interface MenuSection {
+    section: string;
+    items: MenuItem[];
+  }
+  
+  
--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuSection } from '../constants/models/footerdto';
 
 @Component({
   selector: 'app-footer',
@@ -7,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+   menuData: MenuSection[] = [
+    {
+      section: "Find It Fast",
+      items: [
+        { name: "Laptops & Computers", link: "#" },
+        { name: "Cameras & Photography", link: "#" },
+        { name: "Smart Phones & Tablets", link: "#" },
+        { name: "Video Games & Consoles", link: "#" },
+        { name: "TV & Audio", link: "#" },
+        { name: "Gadgets", link: "#" },
+        { name: "Waterproof Headphones", link: "#" }
+      ]
+    },
+    {
+      section: "About",
+      items: [
+        { name: "About", link: "#" },
+        { name: "Contact", link: "#" },
+        { name: "Wishlist", link: "#" },
+        { name: "Compare", link: "#" },
+        { name: "FAQ", link: "#" },
+        { name: "Store Directory", link: "#" }
+      ]
+    },
+    {
+      section: "Customer Care",
+      items: [
+        { name: "My Account", link: "#" },
+        { name: "Track your Order", link: "#" },
+        { name: "Customer Service", link: "#" },
+        { name: "Returns/Exchange", link: "#" },
+        { name: "FAQs", link: "#" },
+        { name: "Product Support", link: "#" }
+      ]
+    }
+  ];
 }
